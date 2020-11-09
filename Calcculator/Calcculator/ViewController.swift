64e17622ad844a2481d194ab480559891cb6e768
Popova-Ir <73823517+Popova-Ir@users.noreply.github.com>
2020-11-09 18:01:26 +0500

    Add files via upload

---
+++ Calcculator/Calcculator/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Calcculator
+//
+//  Created by user on 09/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var operand = ""
+    var numberOne = ""
+    var numberTwo = ""
+    
+ @IBOutlet weak var resultLabel: UILabel!
+    
+ 
+    @IBOutlet weak var inputNmber: UIButton!
+    
+    
+}
